a1ae3cc1f04912d9d7dbd57da4259bfdcb750c7a
YutaFukuda <fukuda@makersmark.co.jp>
2022-07-16 12:46:35 +0900

    ホバーエフェクトサンプル

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import react, {useEffect} from 'react';
 import './App.css';
+import hoverEffect from 'hover-effect';
+import image from './image01.jpg';
+import image2 from './image02.jpg';
+import overlay from './stripe.png'
 
-function App() {
+function App(){
+  useEffect(()=> {
+    var image_animate = new hoverEffect({
+      parent: document.querySelector(".item-image"),
+      intensity: .5,
+      image1: image,
+      image2: image2,
+      displacementImage: overlay
+  })
+  }
+  
+  )
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="item">
+          <div className="item-header">
+            <div className="item-header-heading">AURORA</div>
+            <div className="item-header-heading2">Hover Effect</div>
+          </div>
+          <div className="item-image"></div>
+          <div className="item-footer">
+            <div className="item-footer-quote">Like a sky curtain.</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
